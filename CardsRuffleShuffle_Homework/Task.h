f518c4c9a6d7af44afd853fbd764578d46d91d7b
Valeri Bogoslovov <valeribogoslovov@hotmail.com>
2024-02-15 11:48:51 +0200

    Update Task.h

--- CardsRuffleShuffle_Homework/Task.h
+++ CardsRuffleShuffle_Homework/Task.h
@@ -1,2 +1,7 @@
-Create deck of cards, function to shuffle them and show
-standart and shuffled deck.
+DeckRaffleShuffle
+
+/*Тесте карти
+-метод, който разбърква картите в тестето
+-Картите са клас PlayingCards, включващ type (А,К,3,4....) и suit (spades, diamonds, hearts, clubs)
+-клас DeckOfCards c метод raffleShuffle, който разделя тестето на две части и взима
+по една от всяка купчинка и метод displayDeck*/
